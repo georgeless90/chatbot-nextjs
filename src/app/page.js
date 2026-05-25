a10c9d30703d2208e89b3e03a8321d5f92f899df
Portfolio Hub <georgemmless90@outlook.com>
2026-05-25 15:23:52 -0500

    button upload

--- src/app/page.js
+++ src/app/page.js
@@ -213,17 +213,19 @@ export default function Home() {
           <div className="typing-area">
             <div className="input-field">
               <input type="text" onChange={(e) => setMessage(e.target.value)} value={message} placeholder="Type your message" required />
-              <button type="button" disabled={loading}
-                onClick={sendMessage}>Send</button>
+              
               {selectedFile ? (
               <button type="button" className="button-upload" disabled={loading}
                 onClick={uploadFile}>Send PDF</button>
               ):(<>
-                <input type="file" id="actual-btn" name="selectedFile" onChange={(e) => setSelectedFile(e.target.files[0])} className="button-upload"/>
-                <label htmlFor="actual-btn">Choose File</label>
-              </>
+                  <button type="button" disabled={loading}
+                  onClick={sendMessage}>Send</button>
+                </>
                 
               )}
+
+              <input type="file" id="actual-btn" name="selectedFile" onChange={(e) => setSelectedFile(e.target.files[0])} className="button-upload"/>
+              <label htmlFor="actual-btn">Choose File</label>
               
             </div>
           </div>
